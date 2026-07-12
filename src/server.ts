4344b3072ad0bdf3d772c97bf6efeb63d3a186ba
sxndev <sxnsilva@gmail.com>
2026-07-12 01:00:46 -0300

    configurações express

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const port = 3000
+const app = express()
+
+
+app.get('/movies', (req,res) => {
+  res.send('Listagem de filmes')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`servidor rodando em http://localhost:${port}`)
+})
