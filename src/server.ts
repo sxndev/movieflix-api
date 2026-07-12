9e9eec3dc053157c8148b3143641747add93ef6a
sxndev <sxnsilva@gmail.com>
2026-07-12 03:02:09 -0300

    configurações iniciais do prisma

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,9 @@ import express from 'express'
 const port = 3000
 const app = express()
 
-
 app.get('/movies', (req,res) => {
   res.send('Listagem de filmes')
 })
-
-
 
 app.listen(port, () => {
   console.log(`servidor rodando em http://localhost:${port}`)
