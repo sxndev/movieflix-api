0f3d2ca07a1e00baace3e7f7aef581eac8247008
Nicolas Silva <sxnsilva@gmail.com>
2026-07-23 19:26:29 -0300

    atualização de filmes adicionada

--- src/server.ts
+++ src/server.ts
@@ -21,15 +21,15 @@ app.get("/movies", async (_, res) => {
 });
 
 app.post("/movies", async (req, res) => {
-
   const { title, genre_id, language_id, oscar_count, realease_date } = req.body;
 
   try {
     // case insensitive - se a busca for feita por john wick, John wickl ou JOHN WICK, o registro vai ser retornado na consulta
 
     // case sensitive - se buscar por john wick e no banco estiver como John wick, não vai ser retornado ma consulta
+
     const movieWithSameTitle = await prisma.movie.findFirst({
-      where: { title: { equals: title, mode: "insensitive" } }
+      where: { title: { equals: title, mode: "insensitive" } },
     });
 
     if (movieWithSameTitle) {
@@ -53,6 +53,43 @@ app.post("/movies", async (req, res) => {
     return;
   }
   res.status(201).send("Filme criado");
+});
+
+app.put("/movies/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!movie) {
+      return res.status(404).send({ message: "Filme não encontrado" });
+    }
+    const data = { ...req.body };
+    data.realease_date = data.realease_date
+      ? new Date(data.realease_date)
+      : undefined;
+
+    console.log(data);
+    // pegar o id do registro que será atualizado
+
+    // pegar os dados do filme que será atualizado e atualizar ele no prisma
+    await prisma.movie.update({
+      where: {
+        id,
+      },
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Falha ao atualizar o registro do filme" });
+  }
+  // retornar o status correto informando que o filme foi atualizado
+
+  res.status(200).send("Filme atualizado");
 });
 
 app.listen(port, () => {
