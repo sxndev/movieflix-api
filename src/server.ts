1f652df9ef4ed57ae9ca857d3bd99dceae69ae55
Nicolas Silva <sxnsilva@gmail.com>
2026-07-24 00:32:26 -0300

    filtragem de filmes por gênero adicionada

--- src/server.ts
+++ src/server.ts
@@ -113,6 +113,31 @@ app.delete("/movies/:id", async (req, res) => {
   res.status(200).send("Filme deletado com sucesso");
 });
 
+app.get("/movies/:genreName", async (req, res) => {
+  try {
+    const moviesFilteredByGenreName = await prisma.movie.findMany({
+      include: {
+        genres: true,
+        languages: true,
+      },
+      where: {
+        genres: {
+          name: {
+            equals: req.params.genreName,
+            mode: "insensitive",
+          },
+        },
+      },
+    });
+    res.status(200).send(moviesFilteredByGenreName);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ message: "Falha ao filtrar filmes por gênero" });
+  }
+});
+
 app.listen(port, () => {
   console.log(`servidor rodando em http://localhost:${port}`);
 });
